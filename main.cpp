4ae9aafb4d4d293f44ba40a303b5cf0b670cdc55
lguerra <lguerra@btslivh.eu>
2019-12-12 16:42:29 +0100

    TEST TP05

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 12 décembre 2019, 16:31
+ */
+
+#include <cstdlib>
+using namespace std;
+#include <iostream>
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "test" << endl;
+    return 0;
+}
+
